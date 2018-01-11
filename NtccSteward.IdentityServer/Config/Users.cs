6ff1b71e81490c65523773d3152ca2080bcecb9d
Gary Lima <garylima@outlook.com>
2018-01-10 20:59:07 -0800

    Added NtccSteward.IdentityServer project

---
+++ NtccSteward.IdentityServer/Config/Users.cs
@@ -0,0 +1,25 @@
+﻿using IdentityServer3.Core.Services.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NtccSteward.IdentityServer.Config
+{
+    public static class Users
+    {
+        // For SQL Server implementation, see:
+        // https://stackoverflow.com/questions/31711014/get-identityserver3-to-use-existing-user-sql-database
+        // https://identityserver.github.io/Documentation/docsv2/advanced/userService.html
+        public static List<InMemoryUser> Get()
+        {
+            return new List<InMemoryUser>() {
+                    new InMemoryUser(){
+                        Username="glima",
+                        Password="secret",
+                        Subject="4633E6C1-05EF-43F3-A24A-C5D51BEB840F"  // specific user's unique identifier
+                    }
+            };
+        }
+    }
+}
---
+++ NtccSteward.IdentityServer/Startup.cs
@@ -0,0 +1,43 @@
+﻿using IdentityServer3.Core.Configuration;
+using NtccSteward.IdentityServer.Config;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace NtccSteward.IdentityServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/identity", idSvrApp =>
+            {
+                var idServerServiceFactory = new IdentityServerServiceFactory()
+                    .UseInMemoryClients(Clients.Get())
+                    .UseInMemoryUsers(Users.Get())
+                    .UseInMemoryScopes(Scopes.Get());
+
+                var options = new IdentityServerOptions
+                {
+                    Factory = idServerServiceFactory,
+                    SiteName = "NtccSteward Security Token Service",
+                    IssuerUri = ConfigurationManager.AppSettings["NtccStewardIssuerUri"],  // does not have to be an existing uri
+                    PublicOrigin = ConfigurationManager.AppSettings["NtccStewardStsOrigin"],
+                    SigningCertificate = LoadCertificate()
+                };
+
+                idSvrApp.UseIdentityServer(options);
+            });
+        }
+
+        private X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                $@"{AppDomain.CurrentDomain.BaseDirectory}\certificates\localhost.pfx", "localhost");
+        }
+    }
+}
