0dffdc2f877f03e24498eca6f604a6d969d67380
Gary Lima <garylima@outlook.com>
2018-01-20 22:17:38 -0800

    Added account request form

---
+++ NtccSteward.IdentityServer/appAcct/acctRequestController.js
@@ -0,0 +1,21 @@
+﻿
+
+
+'use strict';
+
+angular.module('appAcct').controller('acctRequestController',
+    ['$scope', '$location', '$log'
+        , function ($scope, $location, $log) {
+
+            var vm = this;
+            vm.config = {};
+            vm.user = {};
+
+            vm.init = function () {
+
+                $location.path("member");
+            };
+
+
+            return vm;
+        }]);
---
+++ NtccSteward.IdentityServer/appAcct/config.js
@@ -0,0 +1,14 @@
+﻿//https://www.jvandemo.com/how-to-configure-your-angularjs-application-using-environment-variables/
+(function (window) {
+
+    window.__config = window.__env || {};
+
+    window.__config.apiUrl = 'https://localhost:44352';
+
+    window.__config.stsUrl = 'https://localhost:44316/';
+
+    window.__config.baseUrl = '/';
+
+    window.__config.enableDebug = true;
+
+})(this);
