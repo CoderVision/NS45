48a2791b50c9b235484a08134305e74cbc0fe28a
Gary Lima <garylima@outlook.com>
2018-03-05 20:15:25 -0800

    Added Web Sockets

---
+++ NtccSteward.Api/Framework/WebSocketHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NtccSteward.Api.Framework
+{
+    public class WebSocketHandler : Microsoft.Web.WebSockets.WebSocketHandler
+    {
+        private static WebSocketCollection _chatClients = new WebSocketCollection();
+        private string _username;
+
+        public WebSocketHandler()
+        {      }
+
+        public override void OnOpen()
+        {
+            _chatClients.Add(this);
+        }
+
+        public override void OnMessage(string message)
+        {
+            _chatClients.Broadcast(message);
+        }
+
+        public override void OnClose()
+        {
+            _chatClients.Remove(this);
+        }
+    }
+}
