0c2dceb5e248566c9258a60d393bdbb1373468d0
CoderVision <garymisc@outlook.com>
2018-06-24 22:22:57 -0700

    Cleaned up uploads controller

--- NtccSteward.Api/Controllers/UploadsController.cs
+++ NtccSteward.Api/Controllers/UploadsController.cs
@@ -9,14 +9,9 @@ using System.Web.Http;
 
 namespace NtccSteward.Api.Controllers
 {
+    [Authorize]
     public class UploadsController : ApiController
     {
-        [HttpGet]
-        public IHttpActionResult Test()
-        {
-            return Ok("Test Response");
-        }
-
         [HttpPost]
         public async Task<HttpResponseMessage> UploadFile()
         {
@@ -27,39 +22,14 @@ namespace NtccSteward.Api.Controllers
                 throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
             }
 
-            string uploadsFolder = System.Web.HttpContext.Current.Server.MapPath("~/uploads");
             string root = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/uploads");
             var provider = new MultipartFormDataStreamProvider(root);
 
-            try
-            {
-                // Reference this for large file uploads:
-                // https://www.strathweb.com/2012/09/dealing-with-large-files-in-asp-net-web-api/
-                var files = await Request.Content.ReadAsMultipartAsync(provider);
-            }
-            catch (Exception ex)
-            {
-                var x = ex;
-            }
+            // Reference this for large file uploads:
+            // https://www.strathweb.com/2012/09/dealing-with-large-files-in-asp-net-web-api/
+            var files = await Request.Content.ReadAsMultipartAsync(provider);
 
-             
-
-            return await Task.FromResult(new HttpResponseMessage());
-
-            //var task = request.Content.ReadAsMultipartAsync(provider).
-            //    ContinueWith<HttpResponseMessage>(o =>
-            //    {
-            //        string file1 = provider.FileData.First().LocalFileName;
-            //        // this is the file name on the server where the file was saved 
-                    
-            //        var fileName = Path.GetFileName(file1);
-
-            //        File.Move(file1, $"{uploadsFolder}\\{fileName}");
-
-            //        return new HttpResponseMessage() { Content = new StringContent("File uploaded.") };
-            //    }
-            //);
-            //return task;
+            return await Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
         }
     }
 }
